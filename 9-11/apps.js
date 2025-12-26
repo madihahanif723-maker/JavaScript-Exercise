bfffcd97752d6516adef78503107a1e93e0a6e2e
Aeiman-Fayyaz <madihahanif723@gmail.com>
2025-12-26 16:03:37 +0500

    practice

---
+++ 9-11/apps.js
@@ -0,0 +1,10 @@
+let question = prompt ("who is the founder of pakistan ")
+let answer = "quaida e azam"
+let score = 0
+if(question=== answer){
+score++
+console.log("correct answer!");
+}
+console.log("Your score is " + score)
+
+
